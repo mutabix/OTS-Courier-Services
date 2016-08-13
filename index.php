16e2fa005e049e839277db11b447bd28ca214988
Matthew Magin <Matthew Magin>
2016-08-13 21:43:29 +1000

    no message

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>On The Spot Couriers - Home</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="shortcut icon" href="images/favicon.ico" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="login_top_bar">
+				<p><a href="#">Facebook</a> | <a href="#">Twitter</a> | <a href="#">My Dashboard</a> | <a href="#">Support</a></p>
+			</div>
+
+			<header>
+				<img src="images/logo.png" alt="On The Spot Couriers">
+
+				<nav id="main_navigation">
+					<ul>
+						<li><a class="active" href="#">Home</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Ship</a></li>
+						<li><a href="#">Track</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</nav>
+			</header>
+			<div id="banner_image">
+				<img src="images/banner.png" alt="Banner Image">
+			</div>
+			<div id="quote_home">
+				<h2>
+					<img src="images/quote_open.png" alt="Quote Open"> &nbsp &nbsp
+					Join hundreds of people and businesses enjoying the efficiency and reliability of OTS Courier Services Today
+					&nbsp &nbsp <img src="images/quote_close.png" alt="Quote Close">
+				</h2>
+			</div>
+			<div id="track_package_home" class="center_100_300_orange_div">
+				<h3 class="heading_white">Track Package</h3>
+				<form>
+					<input id="tracking_number_input_home" type="text">
+					<br />
+					<input type="submit" class="std_button_white" value="Track Package">
+				</form>
+			</div>
+			<div id="quick_links_home">
+				<h3>Quick Links</h3>
+				<div class="columns3">
+					<img src="images/courier.png" alt="Book Shipment">
+					<h4>Book a Pickup</h4>
+
+				</div>
+				<div class="columns3">
+					<img src="images/calculate.png" alt="Calculate Rates">
+					<h4>Get Rates</h4>
+
+				</div>
+				<div class="columns3">
+					<img src="images/dashboard.png" alt="Dashboard">
+					<h4>Dashboard</h4>
+
+				</div>
+				<div class="columns3_last">
+					<img src="images/customer_service.png" alt="Customer Service">
+					<h4>Customer Service</h4>
+
+				</div>
+			</div>
+
+			<footer>
+				<p>© 2016 Geckoboard. All rights reserved.</p>
+			</footer>
+		</div>
+	</body>
+</html>
+
