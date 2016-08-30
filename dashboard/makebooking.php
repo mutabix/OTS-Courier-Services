ca5f639b3247839e9bc9f8cc21915bfba37e2478
Matthew Magin <Matthew Magin>
2016-08-30 16:32:56 +1000

    file storage update

---
+++ dashboard/makebooking.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Dashboard</title>
+
+    <?php include("includes/styling_scripts/meta.html"); ?>
+    <?php include("includes/styling_scripts/css.html"); ?>
+    <?php include("includes/styling_scripts/fonts-icons.html"); ?>
+    <?php include("includes/styling_scripts/javascript.html"); ?>
+</head>
+<body>
+
+<div class="wrapper">
+    <?php include("includes/sidebar.html"); ?>
+
+    <div class="main-panel">
+        <?php include("includes/navbar-mobile.html"); ?>
+
+
+        <div class="content">
+            <div class="container-fluid">
+                
+
+
+
+                
+            </div>
+        </div>
+
+
+        <?php include("includes/footer.html"); ?>
+
+    </div>
+</div>
+</body>
+
+
+</html>
