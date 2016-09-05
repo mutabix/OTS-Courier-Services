e5de63a54543e291a44e285cf2292aedb2457887
Veronica S.Kristiansen <veronk14@student.uia.no>
2016-09-05 10:17:45 +1000

    DB connect is in

--- Tracking.php
+++ Tracking.php
@@ -1,3 +1,7 @@
+<?php
+    include ("../dbTools/dbConnect.php");
+
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -72,24 +76,11 @@ echo "Enter tracking number";
 exit();
 }
 
-$host = "d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-    $dbusername = "tgs66cq1wippa93b";
-    $dbpassword = "xi7mibqw1s765w4q";
-    $dbname = "vrtepy2jtdixgvmr";
+
 $usertable='bookings';
 $location = $_POST['location'];
 $usertable = $_POST['usertable'];
 
-
-try {
-        //DATABASE CONNECTION
-        $dbConnection = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
-        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully"; //Debugging
-    }
-    catch(PDOException $error) { //Should make a error page
-        //echo "Connection failed: " . $error->getMessage(); //Debugging
-    }
 
 $query = "select * from $usertable where Location = '$location'";
 
