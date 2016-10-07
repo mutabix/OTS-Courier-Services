f68abb9b8090adca5748e7d66870336d70327acd
SamL <sweawm@gmail.com>
2016-10-07 14:50:16 +1000

    Package Assignment Update - Added Priority Assignment

---
+++ dashboard/dashboardTools/assignPriority.php
@@ -0,0 +1,36 @@
+<?php
+    include("../../dbTools/dbConnect.php");
+	
+	if(isset($_POST["noPriorityAssigned"])) {
+		$updateDelivery = $dbConnection->prepare("UPDATE deliveries SET priority=:text WHERE deliveryID=:id");
+		$assignment = $_POST["noPriorityAssigned"];
+		$updateDelivery->bindParam(':text', $assignment);
+		$updateDelivery->bindParam(':id', $_POST["packageNum"]);
+		try {
+		$updateDelivery->execute();
+		} catch(Exception $error) {
+			echo 'Exception -> ';
+			var_dump($error->getMessage());
+		}
+	}
+	
+	if(isset($_POST["PriorityReplaced"])) {
+		$updateDelivery = $dbConnection->prepare("UPDATE deliveries SET priority=:text WHERE deliveryID=:id");
+		$assignment = $_POST["PriorityReplaced"];
+		$updateDelivery->bindParam(':text', $assignment);
+		$updateDelivery->bindParam(':id', $_POST["packageNum"]);
+		try {
+		$updateDelivery->execute();
+		} catch(Exception $error) {
+			echo 'Exception -> ';
+			var_dump($error->getMessage());
+		}
+	}
+?>
+
+<!doctype html>
+<html lang="en">
+	<script>
+		window.location = '../assignment.php'
+	</script>
+</html>
