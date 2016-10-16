35879a6b458c4c0dac701eb93acdcfeb208c335c
Hayden Muller <haydenjmuller@gmail.com>
2016-10-17 09:39:42 +1000

    Adding Shift management pages.

---
+++ staff/driverDashboard/shiftManagementDri.php
@@ -0,0 +1,74 @@
+<?php
+    include("../includes/dbTools/dbConnect.php");
+
+    session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<title>Shift Management</title>
+
+    <?php include("includes/styling_scripts/meta.html"); ?>
+    <?php include("includes/styling_scripts/css.html"); ?>
+    <?php include("includes/styling_scripts/fonts-icons.html"); ?>
+    <?php include("includes/styling_scripts/javascript.html"); ?>
+</head>
+<body>
+
+<div class="wrapper">
+    <?php include("includes/sidebar.html"); ?>
+
+    <div class="main-panel">
+        <?php include("includes/navbar-mobile-open.html"); ?>
+        <a class="navbar-brand" href="#">Dashboard</a>
+        <?php include("includes/navbar-mobile-close.html"); ?>
+
+
+        <div class="content">
+            <div class="container-fluid">
+
+              <table class="table table-hover">
+      					<thead>
+      						<tr>
+                    <th>Week</th>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>This Week</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Next Week</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+
+
+
+            </div>
+        </div>
+
+
+        <?php include("includes/footer.html"); ?>
+
+    </div>
+</div>
+</body>
+
+
+</html>
