4ff165d68a5ec3e9b43521dc0e96f269a66e63f7
Matthew Magin <Matthew Magin>
2016-10-21 13:35:21 +1000

    UPDATES TO DASHBOARD

--- dashboard/dashboard2.php
+++ dashboard/dashboard2.php
@@ -2,10 +2,13 @@
     session_start();
     include("../dbTools/dbConnect.php");
     include("../dbTools/checkLogin.php");
+
     $email = $_SESSION['username'];
+
     $getTrackingNumbers = $dbConnection->prepare("SELECT trackingNumber FROM tracking WHERE customer = :customerEmail");
     $getTrackingNumbers->bindParam(':customerEmail', $email);
     $getTrackingNumbers->execute();
+
     $count = 0;
     $pendingCount = 0;
     $readyCount = 0;
@@ -28,31 +31,33 @@
 
         try {
             $getTrackingDetails->execute();
+
         } catch(Exception $error) {
             echo 'Exception -> ';
             var_dump($error->getMessage());
         }
+
         $shipmentStatusCodeFetch = $getTrackingDetails->fetch();
         $shipmentStatusCode = $shipmentStatusCodeFetch['shipmentStatusCode'];
 
         if($shipmentStatusCode == 0){
             $pendingCount++;
-            $trackingStatus[$i] = "Pending";
+            $trackingStatus[$count] = "Pending";
         } else if ($shipmentStatusCode == 1){
             $readyCount++;
-            $trackingStatus[$i] = "Ready for Pickup";
+            $trackingStatus[$count] = "Ready for Pickup";
         } else if ($shipmentStatusCode == 2){
             $processingCount++;
-            $trackingStatus[$i] = "Processing";
+            $trackingStatus[$count] = "Processing";
         } else if ($shipmentStatusCode == 3){
             $deliveryCount++;
-            $trackingStatus[$i] = "Ready for Delivery";
+            $trackingStatus[$count] = "Ready for Delivery";
         } else if ($shipmentStatusCode == 4){
-            $trackingStatus[$i] = "Delivered";
-        }
+            $trackingStatus[$count] = "Delivered";
+        } 
     }
-
-    $notificationResult = $dbConnection->prepare('SELECT * FROM notifications WHERE notificationFor = :email ORDER BY notificationDate DESC LIMIT 5');
+	
+	/*$notificationResult = $dbConnection->prepare('SELECT * FROM notifications WHERE notificationFor = :email ORDER BY notificationDate DESC');
     $notificationResult->bindParam(':email', $_SESSION['username']);
     try {
         $notificationResult->execute();
@@ -60,24 +65,14 @@
     } catch(Exception $error) {
         echo 'Exception -> ';
         var_dump($error->getMessage());
-    }
+    }*/
 
-    //My Bookings Data
-    $myBookingsData = $dbConnection->prepare('SELECT * FROM bookings WHERE senderEmail = :email ORDER BY bookingMadeOn DESC LIMIT 5');
-    $myBookingsData->bindParam(':email', $email);
-    try {
-        $myBookingsData->execute();
-
-    } catch(Exception $error) {
-        echo 'Exception -> ';
-        var_dump($error->getMessage());
-    }
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Dashboard</title>
+	<title>Dashboard</title>
 
     <?php include("includes/styling_scripts/meta.html"); ?>
     <?php include("includes/styling_scripts/css.html"); ?>
@@ -115,7 +110,7 @@
                                 <h4 class="title">Ready for Pickup</h4>
                             </div>
                             <div class="content">
-                            <?php echo "<h4>".$readyCount."</h4>" ?>
+                                <h4>0</h4>
                             </div>
                         </div>
                     </div>
@@ -126,7 +121,7 @@
                                 <h4 class="title">Packages Processing</h4>
                             </div>
                             <div class="content">
-                            <?php echo "<h4>".$processingCount."</h4>" ?>
+                                <h4>0</h4>
                             </div>
                         </div>
                     </div>
@@ -137,101 +132,14 @@
                                 <h4 class="title">Ready for Delivery</h4>
                             </div>
                             <div class="content">
-                            <?php echo "<h4>".$deliveryCount."</h4>" ?>
+                                <h4>0</h4>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div class='row'>
-                    <div class="col-md-12">
-                        <div class="card ">
-                            <div class="header">
-                                <h4 class="title">My Orders</h4>
-                            </div>
-                             <div class="content">
-                                 <div class="table-full-width">
-                                    <table class="table">
-                                        <tbody>
-                                            <tr>
-                                                <td>Booking Date</td>
-                                                <td>Shipment ID</td>
-                                                <td>Sender</td>
-                                                <td>Receiver</td>
-                                                <td>Status</td>
-                                                <td></td>
-                                            </tr>
-                                            <?php
-                                            foreach ($myBookingsData as $booking) {
-                                                
-                                                $shipmentStatusRetrieval = $dbConnection->prepare('SELECT * FROM shipments WHERE trackingNumber = :trackingNumber LIMIT 1');
-                                                $shipmentStatusRetrieval->bindParam(':trackingNumber', $booking['shipmentId']);
-                                                try {
-                                                    $shipmentStatusRetrieval->execute();
-
-                                                } catch(Exception $error) {
-                                                    echo 'Exception -> ';
-                                                    var_dump($error->getMessage());
-                                                }
-
-                                                $shipmentStatus = $shipmentStatusRetrieval->fetch();
-                                                $shipmentStatusCode = $shipmentStatus['shipmentStatusCode'];
-                                                if($shipmentStatusCode == 0){
-                                                    if($shipmentStatus['pendingBool'] == 0){
-                                                        $shipmentStatus = "Pending";
-                                                    } else {
-                                                        $shipmentStatus = "Pending Complete, Waiting for Pickup";
-
-                                                    }
-                                                    $lastActionedDate = $shipmentStatus['pendingDate'];
-
-                                                } else if($shipmentStatusCode == 1){
-                                                    if($shipmentStatus['pickupBool'] == 0){
-                                                        $shipmentStatus = "Driver Enroute to Pickup";
-                                                    } else {
-                                                        $shipmentStatus = "Package Picked Up";
-                                                    }
-
-                                                } else if($shipmentStatusCode == 2){
-                                                    if($shipmentStatus['processingBool'] == 0){
-                                                        $shipmentStatus = "Processing Package";
-                                                    } else {
-                                                        $shipmentStatus = "Processing Complete";
-                                                    }
-
-                                                } else if($shipmentStatusCode == 3){
-                                                    if($shipmentStatus['deliveryBool'] == 0){
-                                                        $shipmentStatus = "Driver Enroute for Delivery";
-                                                    } else {
-                                                        $shipmentStatus = "Package Delivered";
-                                                    }
-                                                }
-
-
-                                                echo '<tr>';
-                                                echo '<td>'.$booking['bookingMadeOn'].'</td>';
-                                                echo '<td>'.$booking['shipmentId'].'</td>';
-                                                echo '<td>'.$booking['senderFirstName']." ".$booking['senderLastName'].'</td>';
-                                                echo '<td>'.$booking['receiverFirstName']." ".$booking['receiverLastName'].'</td>';
-                                                echo '<td>'.$shipmentStatus.'</td>';
-                                                echo '<td>';
-                                                echo '<a href="getBookingDetails.php?id='.$booking['shipmentId'].'" class="btn btn-info" role="button">See More Details</a>';
-                                                echo '</td>';
-
-                                                echo '</tr>';
-                                            }
-                                                
-                                            ?>
-
-                                            <tr><td colspan="6" style='text-align: right;'><a href='seeAllOrders.php'>See All Orders</a></td></tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class='row'>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <div class="card ">
                             <div class="header">
                                 <h4 class="title">Saved Tracking Numbers</h4>
@@ -247,7 +155,7 @@
                                                     echo $trackingNumber[$i];
                                                     echo "</td>";
                                                     echo "<td>";
-                                                    echo $trackingStatus[$i]; //Temp code --> for testing
+                                                    echo "Pending"; //Temp code --> for testing
                                                     echo "</td>";
                                                     echo "<td class='td-actions text-right'>";
                                                         echo "<button type='button' rel='tooltip' title='Edit Task' class='btn btn-info btn-simple btn-xs'>";
@@ -257,8 +165,6 @@
                                                 echo "</tr>";
                                             }
                                             ?>
-
-                                            <tr><td colspan="3" style='text-align: right;'><a href='savedTrackingNumbers.php'>See My Saved Tracking Numbers</a></td></tr>
                                         </tbody>
                                     </table>
                                 </div>
@@ -266,7 +172,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-8">
+                    <div class="col-md-6">
                         <div class="card ">
                             <div class="header">
                                 <h4 class="title">Notifications</h4>
@@ -274,29 +180,34 @@
                             <div class="content">
                                 <div class="table-full-width">
                                     <table class="table">
-                                        <tbody>
-                                            <?php
-                                                foreach ($notificationResult as $notification) {
-                                                    echo '<tr>';
-                                                    echo '<td>';
-                                                    //echo $notification['notificationDate'];
-                                                    $date = date("d/m/Y", strtotime($notification['notificationDate']));
-                                                    echo $date;
+										<thead>
+											<tr>
+												<th class='col-md-1'>Date</th>
+												<th class='col-md-2'>From</th>
+												<th class='col-md-9'>Message</th>
+											</tr>
+										</thead>
+										<tbody>
+											<?php
+												/*foreach ($notificationResult as $notification) {
+													echo '<tr>';
+													echo '<td>';
+													//echo $notification['notificationDate'];
+													$date = date("d/m/Y", strtotime($notification['notificationDate']));
+													echo $date;
 
-                                                    echo '</td>';
-                                                    echo '<td>';
-                                                    echo $notification['notificationFrom'];
-                                                    echo '</td>';
-                                                    echo '<td>';
-                                                    echo $notification['notificationDescription'];
-                                                    echo '</td>';
-                                                    echo '</tr>';
-                                                }
-                                            ?>
-
-                                            <tr><td colspan="3" style='text-align: right;'><a href='notifications.php'>See All Notifications</a></td></tr>
-                                        </tbody>
-                                    </table>
+													echo '</td>';
+													echo '<td>';
+													echo $notification['notificationFrom'];
+													echo '</td>';
+													echo '<td>';
+													echo $notification['notificationDescription'];
+													echo '</td>';
+													echo '</tr>';
+												}
+										echo '</tbody>';
+									echo '</table>';*/
+									?>
                                 </div>
                             </div>
                         </div>
