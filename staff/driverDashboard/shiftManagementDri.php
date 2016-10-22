a3a8de196d452093152c55a5be3b01684fa01e3e
Hayden Muller <haydenjmuller@gmail.com>
2016-10-22 10:50:00 +1000

    Shift management update

--- staff/driverDashboard/shiftManagementDri.php
+++ staff/driverDashboard/shiftManagementDri.php
@@ -1,13 +1,16 @@
 <?php
-    include("../includes/dbTools/dbConnect.php");
-
     session_start();
+    include("../../dbTools/dbConnect.php");
+    //include("checkLogin.php");
+
+    $employeeID = $_SESSION['employeeID'];
+
 ?>
 
 <!doctype html>
 <html lang="en">
 <head>
-	<title>Shift Management</title>
+	<title>Employee Dashboard</title>
 
     <?php include("includes/styling_scripts/meta.html"); ?>
     <?php include("includes/styling_scripts/css.html"); ?>
@@ -28,9 +31,24 @@
         <div class="content">
             <div class="container-fluid">
 
+              <?php
+              $getShiftTimes = $dbConnection->prepare('SELECT * FROM Shift_Management'); //WHERE employeeID = (SOME RANDOM SESSION VARIABLE)
+              try {
+                  $getShiftTimes->execute();
+              } catch(Exception $error) {
+                  echo 'Exception -> ';
+                  var_dump($error->getMessage());
+              }
+
+              $getShiftTimes->bindParam(':employeeID', $employeeID);
+
+              $timetable = $getShiftTimes->fetch();
+
+              ?>
+
               <table class="table table-hover">
-      					<thead>
-      						<tr>
+                <thead>
+                  <tr>
                     <th>Week</th>
                     <th>Monday</th>
                     <th>Tuesday</th>
@@ -39,24 +57,98 @@
                     <th>Friday</th>
                   </tr>
                 </thead>
-                <tbody>
-                  <tr>
-                    <td>This Week</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                  <tr>
-                    <td>Next Week</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                </tbody>
+              </tbody>
+                <?php
+                echo '<tbody>';
+                  echo '<tr>';
+                    echo '<td>This Week</td>';
+                    echo '<td>';
+                    if ($timetable['cur_Mon'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['cur_Tues'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['cur_Wed'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['cur_Thurs'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo '<td>';
+                    if ($timetable['cur_Fri'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                  echo '</tr>';
+                  echo '<tr>';
+                    echo '<td>Next Week</td>';
+                    echo '<td>';
+                    if ($timetable['nex_Mon'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['nex_Tues'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['nex_Wed'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['nex_Thurs'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                    echo '<td>';
+                    if ($timetable['nex_Fri'] = 1)
+                    {
+                      echo 'Rostered On';
+                    }else{
+                      echo 'Not working';
+                    }
+                    echo'</td>';
+                  echo '</tr>';
+                ?></tbody></table>
+
+
+
 
 
 
