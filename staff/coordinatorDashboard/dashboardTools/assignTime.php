5f3a28b7bc2281847610f1b6893e2461ec9408a9
SamL <sweawm@gmail.com>
2016-10-22 11:42:27 +1000

    Delivery Assignment Update

---
+++ staff/coordinatorDashboard/dashboardTools/assignTime.php
@@ -0,0 +1,36 @@
+<?php
+    include("../../../dbTools/dbConnect.php");
+	
+	if(isset($_POST["noTimeAssigned"])) {
+		$updateTime = $dbConnection->prepare("UPDATE deliveries SET deliveryDueBy=:text WHERE deliveryID=:id");
+		$assignment = $_POST["noTimeAssigned"];
+		$updateTime->bindParam(':text', $assignment);
+		$updateTime->bindParam(':id', $_POST["packageNum"]);
+		try {
+		$updateTime->execute();
+		} catch(Exception $error) {
+			echo 'Exception -> ';
+			var_dump($error->getMessage());
+		}
+	}
+	
+	if(isset($_POST["TimeReplaced"])) {
+		$updateTime = $dbConnection->prepare("UPDATE deliveries SET deliveryDueBy=:text WHERE deliveryID=:id");
+		$assignment = $_POST["TimeReplaced"];
+		$updateTime->bindParam(':text', $assignment);
+		$updateTime->bindParam(':id', $_POST["packageNum"]);
+		try {
+		$updateTime->execute();
+		} catch(Exception $error) {
+			echo 'Exception -> ';
+			var_dump($error->getMessage());
+		}
+	}
+?>
+
+<!doctype html>
+<html lang="en">
+	<script>
+		window.location = '../assignment.php'
+	</script>
+</html>
