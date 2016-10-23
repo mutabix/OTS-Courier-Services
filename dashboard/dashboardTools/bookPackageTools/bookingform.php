da3f74b0aded3fc574f26560a76783d13fefd3c6
Reeve Waugh <waughreeve@gmail.com>
2016-10-23 13:47:17 +1000

    Fixes to text

--- dashboard/dashboardTools/bookPackageTools/bookingform.php
+++ dashboard/dashboardTools/bookPackageTools/bookingform.php
@@ -173,7 +173,7 @@
                                     <option>SA</option>
                                     <option>NT</option>
                                     <option>WA</option>
-                                    <option disabled>TAS</option>
+                                    <option disabled>TAS - We do not offer shipping to Tasmania right now</option>
                                 </select>
                             </div>
                         </div>
